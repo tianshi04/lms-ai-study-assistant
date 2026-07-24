e4f1401636572660817bfe7f05cc24e0125cc54b
hoang-01 <nhnochoang456@gmail.com>
2026-07-24 18:52:18 +0700

    style(alembic): format and clean up unused imports in merge migration

--- backend/alembic/versions/b9369500f869_merge_branches.py
+++ backend/alembic/versions/b9369500f869_merge_branches.py
@@ -5,15 +5,13 @@ Revises: 38e60e429f76, 7a8f9e1029ab
 Create Date: 2026-07-24 18:47:04.630265
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from typing import Sequence, Union
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b9369500f869'
-down_revision: Union[str, Sequence[str], None] = ('38e60e429f76', '7a8f9e1029ab')
+revision: str = "b9369500f869"
+down_revision: Union[str, Sequence[str], None] = ("38e60e429f76", "7a8f9e1029ab")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
