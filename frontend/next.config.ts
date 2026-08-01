1c3d5096a07bc0679138dc7895a57551205fff3e
tianshi04 <zomodat123@gmail.com>
2026-08-02 03:06:51 +0700

    feat(frontend): enable Partial Prefetching in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  partialPrefetching: true,
   reactCompiler: true,
   experimental: {
     useTypeScriptCli: true,
